814c1e4dda84f44b474323f4653418290bed954f
Mohammed Shaheen <mshaheen@lab1r2s5.42abudhabi.ae>
2025-08-23 19:43:22 +0400

    well on linux it was bugging and in valgrind there was a invalid read for vector and list(but list was finding the element and prints it fine), solution was sending a & of my array, T& array instead of T array, i guess its copy and it gets destroyed and i be pointing to invalid stuff, it was finding either 0 or some random number, but now fixed, do i need to handle const search about it later

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -5,10 +5,10 @@
 #include <vector>
 #include <list>
 #include <exception>
-
+#include <algorithm>
 
 template<typename T>
-typename T::iterator easyfind(T array, int element){
+typename T::iterator easyfind(T& array, int element){
     typename T::iterator it= std::find(array.begin(), array.end(), element);
     if (it == array.end())
         throw std::runtime_error("Element not found in the container");
